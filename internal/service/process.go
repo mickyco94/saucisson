60fd49e4fa5adef12122340a1957fe8f751f3568
mickyco <mickyclerkinoliver@googlemail.com>
2022-12-02 17:43:41 +0000

    feat: much cleaner checking

--- internal/service/process.go
+++ internal/service/process.go
@@ -1,38 +1,15 @@
 package service
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/mickyco94/saucisson/internal/config"
 	"github.com/mitchellh/go-ps"
 )
 
-// ? I could just give them all IDs :shrug:
-type hashset map[string]processEntry
-
-func (hashset hashset) add(pe processEntry) {
-	hashset[pe.String()] = pe
-}
-
-func (hashset hashset) delete(pe processEntry) {
-	delete(hashset, pe.String())
-}
-
-func (hashset hashset) Contains(pe processEntry) bool {
-	_, exists := hashset[pe.String()]
-	return exists
-}
-
-func (pe processEntry) String() string {
-	//! Super wonky hash
-	return fmt.Sprintf("%v_%v_%v", pe.h, pe.isRunning, pe.listenFor)
-}
-
 type Process struct {
-	entries           map[string][]processEntry
-	liteningForAClose hashset
-	watching          hashset
+	entries  []processEntry
+	watching map[string]struct{}
 }
 
 type processEntry struct {
@@ -44,39 +21,21 @@ type processEntry struct {
 
 func NewProcess() *Process {
 	return &Process{
-		entries:           make(map[string][]processEntry),
-		liteningForAClose: make(hashset),
-		watching:          make(hashset),
+		entries:  make([]processEntry, 0),
+		watching: make(map[string]struct{}),
 	}
 }
 
-// TODO:
-// Executable name is not perfect as a key, since multiple services could be listening
-// to the same process with different executors
-// e.g. onclose do x, onopen do y
-// The service defines the uniqueness, not some parameter
 func (p *Process) HandleFunc(config *config.Process, f func()) {
-	//Some validaiton may be nice :)
 	entry := processEntry{
 		executable: config.Executable,
 		listenFor:  config.State,
-		isRunning:  false, //TODO: This will be set on `Run()`
+		isRunning:  false,
 		h:          f,
 	}
 
-	p.addEntry(config.Executable, entry)
-	p.watching.add(entry)
-}
-
-func (p *Process) addEntry(key string, entry processEntry) {
-	arr, exists := p.entries[key]
-	if !exists {
-		coll := []processEntry{entry}
-		p.entries[key] = coll
-	} else {
-		arr = append(arr, entry)
-		p.entries[key] = arr
-	}
+	p.entries = append(p.entries, entry)
+	p.watching[config.Executable] = struct{}{}
 }
 
 func (entry processEntry) startJob() {
@@ -85,27 +44,9 @@ func (entry processEntry) startJob() {
 
 var PollingInterval = 100 * time.Millisecond
 
-// Let the client decide to run on its own GR
 func (p *Process) Run() error {
-
-	//Initial state
-	processes, _ := ps.Processes()
-	for _, v := range processes {
-		entries, exists := p.entries[v.Executable()]
-
-		for _, entry := range entries {
-			if exists {
-				entry.isRunning = true
-
-			}
-
-		}
-	}
-
 	for {
 		if len(p.entries) == 0 {
-			//Skip if there is zero work to be done
-			//Or return error..?
 			continue
 		}
 
@@ -121,51 +62,28 @@ func (p *Process) Run() error {
 			return err
 		}
 
-		//Split reading and dispatching into two GRs...?
+		runningProcs := make(map[string]struct{})
 
-		hs := make(hashset)
-
-		for _, v := range p.entries {
-			for _, e := range v {
-				if e.listenFor == "close" && e.isRunning {
-					hs.add(e)
-				}
-			}
-		}
-
-		//To allow having multiple keys we could just use an arr with
-		//with executable as a prop on the struct and match against it
-		//...
-		//We could have a hashset of what we are watching to minimise throughput
 		for _, process := range processes {
-			entries := p.entries[process.Executable()]
-
-			for i, entry := range entries {
-				if !entry.isRunning && entry.listenFor == "open" {
-					entry.startJob()
-				} else if entry.isRunning && entry.listenFor == "close" {
-					hs.delete(entry)
-				}
-
-				if !entry.isRunning {
-					entry.isRunning = true
-				}
-
-				entries[i] = entry
-				p.entries[process.Executable()] = entries
+			_, watching := p.watching[process.Executable()]
+			//perf: Micro-optimisation to make runningProcs small as possible
+			if watching {
+				runningProcs[process.Executable()] = struct{}{}
 			}
 		}
 
-		//Any elements of the hs that remain are closed
-		for _, k := range hs {
-			k.startJob()
-			entries := p.entries[k.executable]
-			for i, entry := range entries {
+		for i, entry := range p.entries {
+			_, isRunning := runningProcs[entry.executable]
 
-				entry.isRunning = false
-				entries[i] = entry
-				p.entries[k.executable] = entries
+			if isRunning && entry.listenFor == "open" && !entry.isRunning {
+				entry.startJob()
 			}
+
+			if !isRunning && entry.listenFor == "close" && entry.isRunning {
+				entry.startJob()
+			}
+
+			p.entries[i].isRunning = isRunning
 		}
 
 		<-timer.C
